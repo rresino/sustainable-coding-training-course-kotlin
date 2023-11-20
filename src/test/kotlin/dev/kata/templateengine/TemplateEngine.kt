7729409bb3c786d3f03d45123b9e6d60b8de57a8
Rubén Resino <rresino@gmail.com>
2023-11-20 15:35:26 +0100

    Step 3

--- src/test/kotlin/dev/kata/templateengine/TemplateEngine.kt
+++ src/test/kotlin/dev/kata/templateengine/TemplateEngine.kt
@@ -6,8 +6,8 @@ import kotlin.test.assertEquals
 class TemplateEngine {
 
     companion object {
-        fun foo(template: String, vars: Map<String, String>): String {
-            return template
+        fun foo(template: String?, vars: Map<String, String>?): String {
+            return template?:""
         }
     }
 
@@ -19,22 +19,30 @@ class TemplateEngineShould {
     /*
         TODO-list: especificacion de requisitos.
 
-        null, null -> ???
-        "", [] -> ???
-        "hola", [] -> "hola"
-        "{$var1}", [] -> ???
-        "{$var1}", [$var2: hola] -> ???
-        "hola {$placeholder}", [$placeholder: mundo] -> "hola mundo"
-        "hola {$a1} {$b2} {$a1}", [$a1: foo, $b2: bar] -> "hola foo bar foo"
-        "hola {$a1} {$b2}", [$a1: foo] -> ???
+        1 - null, null -> ""
+        2 - "", [] -> ???
+        3 - "hola", [] -> "hola"
+        4 - "{$var1}", [] -> ???
+        5 - "{$var1}", [$var2: hola] -> ???
+        6 - "hola {$placeholder}", [$placeholder: mundo] -> "hola mundo"
+        7 - "hola {$a1} {$b2} {$a1}", [$a1: foo, $b2: bar] -> "hola foo bar foo"
+        8 - "hola {$a1} {$b2}", [$a1: foo] -> ???
 
      */
     @Test
-    fun foo() {
+    fun `foo should spec 3 - hola return hola`() {
 
         val rs = TemplateEngine.foo("hola", mapOf<String, String>())
 
         assertEquals(rs, "hola")
+
+    }
+
+    @Test
+    fun `foo should spec 1 - null, null returns empty string`() {
+        val rs = TemplateEngine.foo(null, null)
+
+        assertEquals(rs, "")
 
     }
 }
